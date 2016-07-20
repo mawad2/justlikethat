fa8eb0084c682e13f30d9ee01be9a31cfd2aa283
Mohammed Awad <kjjungle0@gmail.com>
2016-07-20 03:54:54 -0400

    Starting files

---
+++ Assets/EventManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void openNext (string nextGameObject){
+
+		gameObject.SetActive (false);
+
+		GameObject next = GameObject.Find (nextGameObject);
+		Debug.Log (next);
+		next.SetActive (true);
+
+		return;
+	}
+}
